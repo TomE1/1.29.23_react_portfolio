2505f613401541d91003a326e7a668ac49b528b7
TomE <tommevans210@gmail.com>
2023-01-29 10:16:40 +0700

    new changes made

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-<h1>yo! I'm pushing this to gh-pages</h1>
+<h1>yo! I'm pushing this to gh-pages. New cha nges made.</h1>
       </header>
     </div>
   );
